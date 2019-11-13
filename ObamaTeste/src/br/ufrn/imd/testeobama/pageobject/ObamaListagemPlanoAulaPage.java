5a25bc678de1b08920b2792e1870c7c77fe52cfe
Shirley Ohara Telemaco de Freitas <shirleyohara@A202-01L.imd.local>
2019-11-13 16:38:19 -0300

    Creating the listing os the classes plan

---
+++ ObamaTeste/src/br/ufrn/imd/testeobama/pageobject/ObamaListagemPlanoAulaPage.java
@@ -0,0 +1,59 @@
+package br.ufrn.imd.testeobama.pageobject;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import br.ufrn.imd.testeobama.pageobject.utils.Utils;
+
+public class ObamaListagemPlanoAulaPage {
+	private WebDriver driver;
+	private static final String PATH_PLANOS = "/planoDeAula/meusPlanosDeAula";
+	
+	@FindBy(className = "tabela-planoDeAula")
+	private WebElement table;
+	
+	private WebElement tbody;
+	
+	/**
+	 * Constructor
+	 * @param driver
+	 */
+	public ObamaListagemPlanoAulaPage (WebDriver driver) {
+		this.driver = driver;
+		driver.get(Utils.URL_BASE + PATH_PLANOS);
+		PageFactory.initElements(this.driver, this);
+		
+		this.tbody = table.findElement(By.tagName("tbody"));
+	}
+	
+	/**
+	 * 
+	 * @param	planName - The name of the plan
+	 * @return 	The row of yhat 
+	 */
+	public WebElement findPlanoDeAula (String planName) {
+		for(WebElement tr: tbody.findElements(By.tagName("tr"))) {
+	    	List<WebElement> tds = tr.findElements(By.tagName("td"));
+	    	if(tds.get(0).getText().contains(planName)) {
+	    		return tr;
+	    	}
+    	}
+		return null;
+	}
+	
+	public boolean inReview (String planName) {
+		for(WebElement tr: tbody.findElements(By.tagName("tr"))) {
+	    	List<WebElement> tds = tr.findElements(By.tagName("td"));
+	    	if(tds.get(0).getText().contains(planName) && 
+	    		tds.get(2).findElement(By.tagName("i")).getAttribute("title").contains("Aguardando")) {
+	    		return true;
+	    	}
+    	}
+		return false;
+	}
+}
---
+++ ObamaTeste/src/br/ufrn/imd/testeobama/pageobject/utils/Utils.java
@@ -0,0 +1,5 @@
+package br.ufrn.imd.testeobama.pageobject.utils;
+
+public class Utils {
+	public static final String URL_BASE = "https://obama.imd.ufrn.br";
+}
