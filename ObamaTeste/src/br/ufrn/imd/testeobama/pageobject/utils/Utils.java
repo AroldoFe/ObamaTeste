471c962afa0f219a1ae813b1ca5034dfe6c7d2fd
Aroldo Felix <junioraroldo37@gmail.com>
2019-11-18 14:01:35 -0300

    update na URL para o dominio de teste.

--- ObamaTeste/src/br/ufrn/imd/testeobama/pageobject/utils/Utils.java
+++ ObamaTeste/src/br/ufrn/imd/testeobama/pageobject/utils/Utils.java
@@ -1,5 +1,5 @@
 package br.ufrn.imd.testeobama.pageobject.utils;
 
 public class Utils {
-	public static final String URL_BASE = "https://obama.imd.ufrn.br";
+	public static final String URL_BASE = "https://hobama.imd.ufrn.br";
 }
